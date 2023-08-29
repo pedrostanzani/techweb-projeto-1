f74b207dd9db45e8d96a70723e23401b6148e9cf
Pedro Stanzani <pedrohstanzani@gmail.com>
2023-08-29 08:29:12 -0300

    Update edit page to only load form when note is successfully fetched

--- src/static/js/edit.js
+++ src/static/js/edit.js
@@ -1,6 +1,28 @@
+const Form = () => {
+  const content = `<input
+      class="form-card-title"
+      type="text"
+      name="titulo"
+      placeholder="Título"
+    />
+    <textarea
+      class="autoresize"
+      name="detalhes"
+      placeholder="Digite o conteúdo..."
+    ></textarea>
+    <button class="btn" type="submit">Editar</button>`
+
+  const element = document.createElement('form');
+  element.method = 'post';
+  element.classList.add('form-card');
+  element.classList.add('form-card-tight');
+  element.innerHTML = content;
+
+  return element
+}
+
 const fetchNote = (noteId) => {
-  const noteTitleInput = document.querySelector('.form-card-title');
-  const noteContentInput = document.querySelector('.autoresize');
+  const formWrapper = document.querySelector('.form-wrapper');
 
   const requestOptions = {
     method: "GET",
@@ -11,6 +33,10 @@ const fetchNote = (noteId) => {
     .then((response) => response.json())
     .then((data) => {
       const { title, content } = data;
+
+      formWrapper.appendChild(Form());
+      const noteTitleInput = document.querySelector('.form-card-title');
+      const noteContentInput = document.querySelector('.autoresize');
       noteTitleInput.value = title;
       noteContentInput.value = content;
     })
